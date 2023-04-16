ad5c2aafcd27975913757d3d59e9b688a4de1687
rkliver <kliver.one.90@gmail.com>
2023-04-16 12:43:23 +0500

    выполнил задание 2.10. Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-
+const getRandomInt = (min,max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0){
+    return -1;
+  }
+  if (max < min){
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const getRandonFloat = (min, max, decimals) => {
+  if (min < 0 || max < 0){
+    return -1;
+  }
+  if (max < min){
+    [min, max] = [max, min];
+  }
+  return (Math.random() * (max - min) + min).toFixed(decimals);
+};
