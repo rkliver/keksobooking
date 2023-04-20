c15c4a5439a29cb9e540b946c624e327fabbed56
rkliver <kliver.one.90@gmail.com>
2023-04-20 23:09:14 +0500

    Выполнил задание 3.11. Больше деталей

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const getRandomInt = (min,max) => {
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.//
 const getRandonFloat = (min, max, decimals) => {
   if (min < 0 || max < 0){
@@ -21,12 +22,15 @@ const getRandonFloat = (min, max, decimals) => {
   const i = (Math.random() * (max - min) + min).toFixed(decimals)
   return Number(i);
 };
+
 //Функция, возвращающая случайный элемент массива.//
 const getRandomArrayElement = (elements) => {
   return elements[getRandomInt(0, elements.length - 1)];
 };
+
 //Данные из ДЗ.//
 const ADS_NEARBY_COUNT = 10;
+
 const TITLES = [
   'Лучшее предложение в вашей жизни!',
   'Только сегодня и только для Вас!',
@@ -34,22 +38,26 @@ const TITLES = [
   'Только кошачьим! С людьми нельзя',
   'Вы не пожалеете!',
 ];
+
 const TYPES = [
   'palace',
   'flat',
   'house ',
   'bungalow',
 ];
+
 const CHECKINS = [
   '12:00',
   '13:00',
   '14:00',
 ];
+
 const CHECKOUTS = [
   '12:00',
   '13:00',
   '14:00',
 ];
+
 const FEATURES = [
   'wifi',
   'dishwasher',
@@ -58,35 +66,57 @@ const FEATURES = [
   'elevator',
   'conditioner',
 ];
+
 const DESCRIPTIONS = [
   'Уютное жильё',
   'Лучший дом в вашей жизни',
   'Вы почувствуете себя как дома',
 ];
+
 const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
-//Создает "ссылку" на avatar//
-const createAvatarParth = () =>{
-  let avatarParths = [];
-  for (let i = 0; i < ADS_NEARBY_COUNT; i ++) {
-    if (i < 9){
-      avatarParths[i] ='img/avatars/user' + '0' + (i +1) + '.png';
-    } else {
-      avatarParths[i] = 'img/avatars/user10.png';
-    }
+
+//массив "ссылок" на avatar//
+let avatarParths = [];
+for (let i = 0; i < ADS_NEARBY_COUNT; i ++) {
+  if (i < ADS_NEARBY_COUNT - 1){
+    avatarParths[i] ='img/avatars/user' + '0' + (i +1) + '.png';
+  } else {
+    avatarParths[i] = 'img/avatars/user10.png';
   }
-  return avatarParths[getRandomInt(0,9)];
+}
+
+//создает массив строк "photos"//
+const getPhotos = () => {
+  const photos = [];
+  for (let i = 0; i < getRandomInt(1,ADS_NEARBY_COUNT); i ++) {
+    photos.push(getRandomArrayElement(PHOTOS));
+  }
+  return photos;
 };
+
+//создает массив строк "features"//
+const getFeatures = () => {
+  const features = [];
+  for (let i = 0; i < getRandomInt(1,ADS_NEARBY_COUNT); i ++) {
+    features.push(getRandomArrayElement(FEATURES));
+  }
+  const uniqueFeatures = features.filter((feature, index) => {
+    return features.indexOf(feature) == index;
+  });
+  return uniqueFeatures;
+};
+
 //Создаёт "Объявление неподалёку"//
 const createAdNearby = () => {
   const x = getRandonFloat(35.65000, 35.70000, 5);
   const y = getRandonFloat(139.70000 , 139.80000, 5);
   return {
     author : {
-      avatar: createAvatarParth(),//Адреса изображений не повторяются.
+      avatar: avatarParths.shift(),
     },
     offer : {
       title : getRandomArrayElement(TITLES),
@@ -97,9 +127,9 @@ const createAdNearby = () => {
       guests : getRandomInt(1,30),
       checkin : getRandomArrayElement (CHECKINS),
       checkout : getRandomArrayElement(CHECKOUTS),
-      features : getRandomArrayElement(FEATURES),//массив строк — массив случайной длины из значений.Значения не должны повторяться.
+      features : getFeatures(),
       description : getRandomArrayElement(DESCRIPTIONS),
-      photos : getRandomArrayElement(PHOTOS),//массив строк — массив случайной длины из значений
+      photos : getPhotos(),
 
     },
     location : {
@@ -108,5 +138,5 @@ const createAdNearby = () => {
     },
   };
 };
+
 const AdsNearby = new Array(ADS_NEARBY_COUNT).fill(null).map( () => createAdNearby());
-console.log(AdsNearby);
